8b0adea53f56dcdb8ba151cd0dbb1d79022ff2b8
Karen-Karapetyan-20 <61345684+Karen-Karapetyan-20@users.noreply.github.com>
2020-05-17 10:37:28 +0400

    Update app.py

--- Socnet with Flask/socnet/app.py
+++ Socnet with Flask/socnet/app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config["SECRET_KEY"] = "key"
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://postgres:karapetyan20@localhost/socnet"
+app.config["SECRET_KEY"] = "your_key"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://postgres:your_password@localhost/socnet"
 
 db = SQLAlchemy(app)
